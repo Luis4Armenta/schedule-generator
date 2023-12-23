8b25778f97a077de1462bf5745c3fe44abe0dfa0
Luis Angel Armenta Delgadillo <luigiangelxd@gmail.com>
2023-12-23 00:16:21 -0600

    Tests

--- src/pages/generator/Components/viwer/schedule/Session.test.js
+++ src/pages/generator/Components/viwer/schedule/Session.test.js
@@ -9,25 +9,63 @@ describe('Session Component', () => {
       bg: 'bg-primary',
       text: 'text-white',
     },
-    subject: 'Comunicación de datos',
     sequence: 'ABC123',
     teacher: 'Susy Cuevas Escobar',
     positiveScore: 0.7,
+    show: true,
+    contenido: 'Comunicación de datos',
+    dia: 'Lunes',
+    inicio: '07:00',
+    fin: '09:00',
+    subject: 'Comunicación de datos',
   };
 
   it('renders without crashing', () => {
-    render(<Session session={mockSession} />);
+    render(<table><tbody><tr><Session session={mockSession} /></tr></tbody></table>);
     expect(screen.getByText('Comunicación de datos')).toBeInTheDocument();
-    // You can add more specific assertions here based on your component structure
   });
 
   it('renders session information correctly', () => {
-    render(<Session session={mockSession} />);
+    render(<table><tbody><tr><Session session={mockSession} /></tr></tbody></table>);
     expect(screen.getByText('Comunicación de datos')).toBeInTheDocument();
     expect(screen.getByText('ABC123')).toBeInTheDocument();
     expect(screen.getByText('Susy Cuevas Escobar')).toBeInTheDocument();
-    expect(screen.getByText('0.70')).toBeInTheDocument(); // Assuming your positiveScore is 0.7
+    expect(screen.getByText('0.70')).toBeInTheDocument();
   });
 
-  // Add more test cases as needed based on your component's behavior
+  it('renders Session component with correct rowSpan value', () => {
+    const example = {
+      color: {
+        bg: 'bg-primary',
+        text: 'text-white',
+      },
+      sequence: 'ABC123',
+      teacher: 'Susy Cuevas Escobar',
+      positiveScore: 0.7,
+      show: true,
+      contenido: 'Comunicación de datos',
+      dia: 'Lunes',
+      inicio: '07:00',
+      fin: '09:00',
+      subject: 'Comunicación de datos',
+    };
+
+    const tests = [
+      {session: {...example, inicio: '07:00', fin: '07:30'}, expected: 1},
+      {session: {...example, inicio: '07:00', fin: '08:00'}, expected: 2},
+      {session: {...example, inicio: '07:00', fin: '08:30'}, expected: 3},
+      {session: {...example, inicio: '07:00', fin: '09:00'}, expected: 4},
+      {session: {...example, inicio: '07:00', fin: '09:30'}, expected: 5},
+      {session: {...example, inicio: '07:00', fin: '10:00'}, expected: 6},
+      {session: {...example, inicio: '07:00', fin: '10:30'}, expected: 7},
+    ];
+
+    tests.forEach(({session, expected}) => {
+      const { container } = render(<table><tbody><tr><Session session={session} /></tr></tbody></table>);
+      const sessionElement = container.querySelector('td');
+      const actualRowSpan = sessionElement.getAttribute('rowSpan');
+
+      expect(actualRowSpan).toBe(expected.toString());
+    });
+  });
 });
