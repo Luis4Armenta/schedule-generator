4b06c74ce8a6a30f5c53a03c7b41f093a8265d72
Luis Armenta <luigiangelxd@gmail.com>
2024-07-13 03:11:50 -0600

    refactor: mejora de rendimiento para mostrar horarios

--- src/pages/generator/Components/viwer/schedule/Schedule.js
+++ src/pages/generator/Components/viwer/schedule/Schedule.js
@@ -1,100 +1,33 @@
 import React, { forwardRef } from 'react'
 import Session from './Session';
-import transformCourses from "./utils";
+import getEventsFromCourses from "./utils";
 
 const Schedule = forwardRef((props, ref) => {
-  const diasSemana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-  const horasDia = [
+  const {selectedSchedule} = props 
+
+  const hours = [
     '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
     '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
     '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30',
     '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'
   ];
-  const {selectedSchedule} = props 
+  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];  
 
  
-  let courses = []
+  let events = []
   if (selectedSchedule && selectedSchedule.courses) {
-    courses = transformCourses(selectedSchedule.courses);
+    events = getEventsFromCourses(selectedSchedule.courses);
   }
-
-  // Datos de eventos
-  let eventos = [
-  ];
-
-  const colores = [
-    { bg: 'bg-primary', text: 'text-white' },
-    { bg: 'bg-secondary', text: 'text-white' },
-    { bg: 'bg-success', text: 'text-black' },
-    { bg: 'bg-danger', text: 'text-white' },
-    { bg: 'bg-warning', text: 'text-black' },
-    { bg: 'bg-info', text: 'text-white' },
-    { bg: 'bg-light', text: 'text-black' },
-    { bg: 'bg-dark', text: 'text-white' },
-    { bg: 'bg-success-subtle', text: 'text-success-emphasis' },
-    { bg: 'bg-danger-subtle', text: 'text-danger-emphasis' },
-    { bg: 'bg-warning-subtle', text: 'text-warning-emphasis' },
-    { bg: 'bg-info-subtle', text: 'text-info-emphasis' },
-  ];
 
   // Función para verificar si hay un evento en un día y hora específicos
-  const obtenerEvento = (dia, hora) => {
-    return eventos.find(e => e.dia === dia && e.inicio <= hora && e.fin > hora);
+  const getEvent = (day, hour) => {
+    return events.find(e => e.dia === day && e.inicio <= hour && e.fin > hour);
   };
 
-
-  function obtenerColorAleatorio() {
-    const coloresDisponibles = colores.filter(color => !color.usado);
-  
-    if (coloresDisponibles.length === 0) {
-      colores.forEach(color => color.usado = false);
-    }
-  
-    const colorSeleccionado = coloresDisponibles[Math.floor(Math.random() * coloresDisponibles.length)];
-    colorSeleccionado.usado = true;
-  
-    return colorSeleccionado;
-  }
-
-  let coursesProps = {}
-  const loadEvents = () => {
-    
-    for (const course of courses) {
-        let props = coursesProps[course.subject];
-
-        if (!props) {
-          const color = obtenerColorAleatorio()
-          coursesProps[course.subject] = {
-            color: color,
-            showing: false
-          }
-          props = coursesProps[course.subject];
-        }
-
-
-        for (const session of course.sessions) {
-          eventos = eventos.concat({
-            color: props.color,
-            show: props.showing,
-            contenido: course.subject,
-            dia: session.day,
-            inicio: session.start,
-            fin: session.end,
-            teacher: course.teacher,
-            subject: course.subject,
-            sequence: course.sequence,
-            positiveScore: course.positiveScore,
-            availability: course.course_availability,
-          })
-        }
-      }
-    }
-
-  loadEvents();
   return (
     <div className="container-fluid">
       <div className="table-responsive overflow-auto m-3" style={{height: '60vh'}}>
-        {courses.length === 0 &&
+        {events.length === 0 &&
           <div className='w-100 h-100 rounded border-dark-subtle text-center d-flex flex-column align-items-center justify-content-center text-body-secondary' style={{border: '2px dashed'}}>
             
             <i className="bi bi-calendar4-week fs-1"></i>
@@ -102,23 +35,23 @@ const Schedule = forwardRef((props, ref) => {
           </div>
         }
 
-        {courses.length !== 0 && <table className="table table-bordered" style={{tableLayout: 'fixed'}} ref={ref}>
+        {events.length !== 0 && <table className="table table-bordered" style={{tableLayout: 'fixed'}} ref={ref}>
           <thead>
             <tr>
               <th className='text-center'>
                 Hora
               </th>
-              {diasSemana.map((dia, index) => (
+              {days.map((dia, index) => (
                 <th key={dia} className='text-center fs-6'>{dia}</th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {horasDia.map((hora) => (
+            {hours.map((hora) => (
               <tr key={hora} style={{height: '10px'}}>
                 <th key={hora} className='text-center fs-6'>{hora}</th>
-                {diasSemana.map((dia) => {
-                  const evento = obtenerEvento(dia, hora);
+                {days.map((dia) => {
+                  const evento = getEvent(dia, hora);
                   if (evento) {
                     if (evento.show) {
                       return null;
--- src/pages/generator/Components/viwer/schedule/utils.js
+++ src/pages/generator/Components/viwer/schedule/utils.js
@@ -1,5 +1,31 @@
-function transformCourses(inputCourses) {
-  return inputCourses.map(course => {
+function getEventsFromCourses(inputCourses) {
+  const events = [];
+  const colors = [
+    { bg: 'bg-primary', text: 'text-white' },
+    { bg: 'bg-secondary', text: 'text-white' },
+    { bg: 'bg-success', text: 'text-black' },
+    { bg: 'bg-danger', text: 'text-white' },
+    { bg: 'bg-warning', text: 'text-black' },
+    { bg: 'bg-info', text: 'text-white' },
+    { bg: 'bg-light', text: 'text-black' },
+    { bg: 'bg-dark', text: 'text-white' },
+    { bg: 'bg-success-subtle', text: 'text-success-emphasis' },
+    { bg: 'bg-danger-subtle', text: 'text-danger-emphasis' },
+    { bg: 'bg-warning-subtle', text: 'text-warning-emphasis' },
+    { bg: 'bg-info-subtle', text: 'text-info-emphasis' },
+  ];
+
+  inputCourses.forEach(course => {
+    const availableColors = colors.filter(color => !color.usado);
+
+    // Si se acaban los colores se repiten
+    if (availableColors.length === 0) {
+      colors.forEach(color => color.usado = false);
+    }
+
+    const selectedColor = availableColors[Math.floor(Math.random() * availableColors.length)];
+    selectedColor.usado = true;
+
     const { teacher, subject, teacher_positive_score, schedule, sequence, course_availability } = course;
 
     const daysMapping = {
@@ -10,27 +36,23 @@ function transformCourses(inputCourses) {
       Friday: 'Viernes',
     };
 
-    const sessions = [];
     schedule.forEach(session => {
-      sessions.push({
-        day: daysMapping[session.day],
-        start: session.start_time,
-        end: session.end_time,
-      })
-
+      events.push({
+        dia: daysMapping[session.day],
+        inicio: session.start_time,
+        fin: session.end_time,
+        
+        color: selectedColor,
+        show: false,
+        teacher,
+        subject,
+        sequence,
+        positiveScore: teacher_positive_score,
+        availability: course_availability,
+      });
     });
-
-    const positiveScore = teacher_positive_score;
-
-    return {
-      subject,
-      sequence,
-      teacher,
-      positiveScore, 
-      sessions,
-      course_availability
-    }
-  });
+  })
+  return events;
 }
 
-export default transformCourses;
+export default getEventsFromCourses;
