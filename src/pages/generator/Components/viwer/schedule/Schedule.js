347286dea140036f803a4ac073e7f6bb07fc27fb
Luis Armenta <luigiangelxd@gmail.com>
2024-07-13 04:41:55 -0600

    refactor: ahora el horario se corta cuando se lleva a la hora del ultimo evento

--- src/pages/generator/Components/viwer/schedule/Schedule.js
+++ src/pages/generator/Components/viwer/schedule/Schedule.js
@@ -2,11 +2,9 @@ import React, { forwardRef } from 'react';
 import Session from './Session';
 import getEventsFromCourses from "./utils";
 
-// Componente Schedule que muestra el horario
 const Schedule = forwardRef((props, ref) => {
-  const { selectedSchedule } = props; // Obtener el horario seleccionado de las props
+  const { selectedSchedule } = props;
 
-  // Lista de horas en incrementos de 30 minutos
   const hours = [
     '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
     '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
@@ -14,23 +12,33 @@ const Schedule = forwardRef((props, ref) => {
     '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00'
   ];
 
-  // Lista de días de la semana
   const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
 
-  // Obtener los eventos y atributos adicionales del horario seleccionado
   const { schedule, extraAttributes } = getEventsFromCourses(selectedSchedule ? selectedSchedule.courses : []);
 
-  // Función para calcular el rowspan de una celda basada en la duración del evento
   const getRowspan = (start, end) => {
     const startIndex = hours.indexOf(start);
     const endIndex = hours.indexOf(end);
     return endIndex - startIndex;
   };
 
+  // Encuentra la última hora con eventos
+  let lastHourIndex = -1;
+  for (let i = hours.length - 1; i >= 0; i--) {
+    if (days.some(day => schedule[i][days.indexOf(day)] !== null)) {
+      lastHourIndex = i;
+      break;
+    }
+  }
+
+  // Si no hay eventos, muestra solo hasta la primera hora
+  if (lastHourIndex === -1) {
+    lastHourIndex = 0;
+  }
+
   return (
     <div className="container-fluid">
       <div className="table-responsive overflow-auto m-3" style={{ height: '60vh' }}>
-        {/* Mostrar mensaje si no hay horarios seleccionados */}
         {(!selectedSchedule || selectedSchedule.courses.length === 0) &&
           <div className='w-100 h-100 rounded border-dark-subtle text-center d-flex flex-column align-items-center justify-content-center text-body-secondary' style={{ border: '2px dashed' }}>
             <i className="bi bi-calendar4-week fs-1"></i>
@@ -38,7 +46,6 @@ const Schedule = forwardRef((props, ref) => {
           </div>
         }
 
-        {/* Mostrar tabla de horarios si hay cursos seleccionados */}
         {selectedSchedule && selectedSchedule.courses.length !== 0 && (
           <table className="table table-bordered" style={{ tableLayout: 'fixed' }} ref={ref}>
             <thead>
@@ -50,7 +57,7 @@ const Schedule = forwardRef((props, ref) => {
               </tr>
             </thead>
             <tbody>
-              {hours.map((hour, hourIndex) => (
+              {hours.slice(0, lastHourIndex + 1).map((hour, hourIndex) => (
                 <tr key={hour} style={{ height: '10px' }}>
                   <th key={hour} className='text-center fs-6'>{hour}</th>
                   {days.map((day, dayIndex) => {
