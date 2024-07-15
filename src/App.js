6c103ee0ebfc11b8f18cd99314d01685c1dfd9d2
Luis Armenta <luigiangelxd@gmail.com>
2024-07-15 13:44:52 -0600

    feat: visualizador de comentarios básico

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes} from 'react-router-dom'
 import HomePage from "./pages/homepage/homepage";
 import Generator from './pages/generator/Generator';
+import Profesor from './pages/profesor/Profesor';
 import { HashRouter } from 'react-router-dom';
 
 import './app.css'
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' exact Component={HomePage}/>
           <Route path='/generator' Component={Generator} />
+          <Route path='/profesor/:name' Component={Profesor} />
         </Routes>
       </HashRouter>
   );
